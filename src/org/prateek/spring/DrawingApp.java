9f8a490a37d4681a99b3315651fc5bcce5eef665
prateeka <prateeka@hp.com>
2014-05-27 03:46:33 -0700

    First spring app with application context

---
+++ src/org/prateek/spring/DrawingApp.java
@@ -0,0 +1,28 @@
+// DrawingApp.java - (insert one line description here)
+// (C) Copyright 2014 Hewlett-Packard Development Company, L.P.
+
+package org.prateek.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ */
+public class DrawingApp
+{
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args)
+    {
+
+        final ApplicationContext appContext = new ClassPathXmlApplicationContext("spring.xml");
+
+        final Triangle triangle = (Triangle) appContext.getBean("triangle");
+
+        triangle.draw();
+
+    }
+}
---
+++ src/org/prateek/spring/Triangle.java
@@ -0,0 +1,15 @@
+// Triangle.java - (insert one line description here)
+// (C) Copyright 2014 Hewlett-Packard Development Company, L.P.
+
+package org.prateek.spring;
+
+/**
+ * 
+ */
+public class Triangle
+{
+    public void draw()
+    {
+        System.out.println("Triangle drawn");
+    }
+}
