d70493819286627d675fb163ab9484e990e7fe97
prateeka <prateeka@hp.com>
2014-05-29 17:27:15 -0700

    Constructor based application context

--- src/org/prateek/spring/Triangle.java
+++ src/org/prateek/spring/Triangle.java
@@ -8,8 +8,51 @@ package org.prateek.spring;
  */
 public class Triangle
 {
+    private String type;
+    private int height;
+
+    public Triangle(final String type)
+    {
+        this.type = type;
+    }
+
+    public Triangle(final int height)
+    {
+        this.height = height;
+    }
+
+    public Triangle(final String type, final int height)
+    {
+        this.type = type;
+        this.height = height;
+    }
+
+    /**
+     * @return the height
+     */
+    public int getHeight()
+    {
+        return height;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType()
+    {
+        return type;
+    }
+
     public void draw()
     {
-        System.out.println("Triangle drawn");
+        System.out.println("Triangle drawn with type: " + getType() + "with height: " + getHeight());
     }
+
+    //    /**
+    //     * @param type the type to set
+    //     */
+    //    public void setType(final String type)
+    //    {
+    //        this.type = type;
+    //    }
 }
